3378d7d4eaf316d6a1c881bdf660b37105a86cd6
Specxyy <shamnadthayyil8@gmail.com>
2024-11-23 13:22:37 +0530

    Login Fixed

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,62 +1,82 @@
-import { useState } from 'react';
-import Logo from '../../olx-logo.png';
-import './Login.css';
-import { useNavigate } from 'react-router-dom';
-import { useFirebase } from '../../store/FirebaseContext';
-import { signInWithEmailAndPassword } from 'firebase/auth';
+import { useState } from "react";
+import Logo from "../../assets/loginEntryPointChat.webp";
+import "./Login.css";
+import { Link, useNavigate } from "react-router-dom";
+import { useFirebase } from "../../store/FirebaseContext";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { toast } from "react-toastify";
 
 function Login() {
   const navigate = useNavigate();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
 
-  const {auth} =  useFirebase();
+  const { auth } = useFirebase();
 
-  const handleLogin = async(e) => {
-    try {
-      e.preventDefault();
-
-      await signInWithEmailAndPassword(auth, email, password)
-
-      setEmail('');
-      setPassword('');
-      
-      navigate('/');
-    } catch (error) {
-      console.log("Error occur while login", error)
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    
+    if (!email || !password) {
+      toast.warning("All fields are required.")
+      return;
     }
-  }
+    
+    setLoading(true);
+
+    try {
+
+      await signInWithEmailAndPassword(auth, email, password);
+
+      setEmail("");
+      setPassword("");
+
+      navigate("/");
+      toast.success("Login Successfull.")
+    } catch (error) {
+      toast.error("Login failed. Please try again.",error);
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <div>
       <div className="loginParentDiv">
-        <img width="200px" height="200px" src={Logo}></img>
-        <form onSubmit={handleLogin}>
-          <label htmlFor="fname">Email</label>
-          <br />
-          <input
-            className="input"
-            type="email"
-            id="fname"
-            name="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <br />
-          <label htmlFor="lname">Password</label>
-          <br />
-          <input
-            className="input"
-            type="password"
-            id="lname"
-            name="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <br />
-          <br />
-          <button type='submit'>Login</button>
-        </form>
-        <a>Signup</a>
+        <div className="greet">
+          <img width="200px" height="200px" src={Logo} />
+          <p>Help us become one of the safest places to buy and sell</p>
+        </div>
+        <div className="form-ctn">
+          <form onSubmit={handleLogin}>
+            <div className="formx">
+            <label htmlFor="fname">Email</label>
+            <br />
+            <input
+              className="input"
+              type="email"
+              id="fname"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <br />
+            <label htmlFor="lname">Password</label>
+            <br />
+            <input
+              className="input"
+              type="password"
+              id="lname"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <br />
+            <br />
+            </div>
+            <button type="submit">{loading ? "Loging...": "Login"}</button>
+          </form>
+          <Link to="/signup" className="signup-link">Signup</Link>
+        </div>
       </div>
     </div>
   );
